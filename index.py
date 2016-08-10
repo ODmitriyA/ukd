c0e6a27ad5fa9ff620a6292a2c247ec2aa0a3b2c
odmitriya <dmitriyxt5@gmail.com>
2016-08-10 09:25:22 +0500

    OK

--- index.py
+++ index.py
@@ -31,9 +31,9 @@ def sendToFtp():
         cfg.read('config.ini')
     else:
         print('Отсутствует файл настроек!')
-    host = cfg['FTP']['host']
-    user = cfg['FTP']['user']
-    passwd = cfg['FTP']['passwd']
+    host = cfg['FTP']['host'][1:-1]
+    user = cfg['FTP']['user'][1:-1]
+    passwd = cfg['FTP']['passwd'][1:-1]
     dsk = ftplib.FTP(
         host=host,
         user=user,
